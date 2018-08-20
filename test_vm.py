3b2ec0fb46d0081198405dd5e1c7a60333fb12a3
aeranghang <aeranghang@gmail.com>
2018-08-20 12:04:13 +0900

    음료 뽑기

--- test_vm.py
+++ test_vm.py
@@ -16,6 +16,24 @@ def test_accumulation_of_change():
     m.run("동전 100")
     assert "잔액은 200원입니다" == m.run("잔액")
 
+def test_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "커피가 나왔습니다" == m.run("음료 커피")
+    assert "잔액은 350원입니다" == m.run("잔액")
+
+def test_모르는_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "알 수 없는 음료입니다" == m.run("음료 맥주")
+    assert "잔액은 500원입니다" == m.run("잔액")
+
+def test_동전이_부족한_상황에서_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 100")
+    assert "잔액이 부족합니다" == m.run("음료 커피")
+    assert "잔액은 100원입니다" == m.run("잔액")
+
 def test_unknown_command():
     m = VendingMachine()
     assert "알 수 없는 명령입니다" == m.run("웅앵")
